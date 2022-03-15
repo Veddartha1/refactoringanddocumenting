d501f373634c4a179136b3ffb865146192182453
Sergio PM <spayam82@gmail.com>
2022-03-15 20:45:55 +0100

    [MOD] extraure constant

--- src/main/java/es/cipfpbatoi/ed/refactoringanddocumenting/Estudiante.java
+++ src/main/java/es/cipfpbatoi/ed/refactoringanddocumenting/Estudiante.java
@@ -2,6 +2,7 @@ package es.cipfpbatoi.ed.refactoringanddocumenting;
 
 public class Estudiante{
 	
+	private static final int MAJORIA_EDAT = 18;
 	int edad;
 	String nombre;
 	
@@ -24,7 +25,7 @@ public class Estudiante{
 
 	public static boolean esMayorDeEdad(int edad)
 	{
-		return edad >= 18;
+		return edad >= MAJORIA_EDAT;
 	}
 	
 }
